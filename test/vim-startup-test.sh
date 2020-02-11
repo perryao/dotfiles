bed0daa3fa55a67c573bc0f5256034b9562c380c
Mike Perry <mike@mikeperry.io>
2020-02-11 00:11:38 -0500

    use absolute path to reference vim-startuptime

--- test/vim-startup-test.sh
+++ test/vim-startup-test.sh
@@ -3,8 +3,8 @@
 # 1. install all plugins (pipe to /dev/null because fzf.vim is noisy)
 nvim -u init.vim --headless +PlugInstall +qall &> /dev/null
 # 2. test base startuptime
-n1=`./go/bin/vim-startuptime -vimpath nvim | grep "^Total Average:" | cut -d ' ' -f3`
+n1=`/root/go/bin/vim-startuptime -vimpath nvim | grep "^Total Average:" | cut -d ' ' -f3`
 # 3. test startuptime with all plugins
-n2=`./go/bin/vim-startuptime -vimpath nvim -- -u init.vim | grep "^Total Average:" | cut -d ' ' -f3`
+n2=`/root/go/bin/vim-startuptime -vimpath nvim -- -u init.vim | grep "^Total Average:" | cut -d ' ' -f3`
 # 4. compute percent increase
 awk -v n1=$n1 -v n2=$n2 'BEGIN {printf "Percent increase in startup time = %s%%\n", (((n2 - n1) / n1)*100) }'
